4dd44d81103e9d187a9c344b858580bd29defbd5
ozandsahintas <ozan.deniz.sahintas@gmail.com>
2024-02-05 00:09:47 +0300

    Initial & Singleton GM & Simple API

---
+++ sup-traders/Controllers/ExchangeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sup_traders.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ExchangeController(ILogger<ExchangeController> logger) : ControllerBase
+    {
+        private readonly ILogger<ExchangeController> _logger = logger;
+        private static readonly GameManager gm = GameManager.GetInstance();
+
+        [HttpPost]
+        public void Buy()
+        {
+            _logger.Log(LogLevel.Information, gm.Test());
+            return;
+        }
+
+
+        [HttpPost]
+        public void Sell()
+        {
+            _logger.Log(LogLevel.Information, "Sell!");
+            return;
+        }
+    }
+}
---
+++ sup-traders/GameManager.cs
@@ -0,0 +1,21 @@
+﻿namespace sup_traders
+{
+    public sealed class GameManager
+    {
+        public GameManager() { }
+        private static GameManager? _instance;
+
+        public static GameManager GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new GameManager();
+            }
+            return _instance;
+        }
+        public string Test()
+        {
+            return "Hey";
+        }
+    }
+}
